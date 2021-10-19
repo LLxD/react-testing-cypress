2a4864e6bacb1ae09a57e611372ca1c817828897
Lucas Lima <limalucas454@gmail.com>
2021-10-19 15:36:35 -0300

    finalizing my first real test

--- cypress/integration/Navigation.test.tsx
+++ cypress/integration/Navigation.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from '@cypress/react';
 
-it('Goes to offpremium site and clicks a button!', () => {
+it('Goes to offpremium site and tries to login', () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
         // returning false here prevents Cypress from
         // failing the test
@@ -10,4 +10,6 @@ it('Goes to offpremium site and clicks a button!', () => {
 cy.viewport(1280,720)
 cy.visit('/');
 cy.get('.newsletter__popup > .sc-bdnxRM > .sc-hKFxyN > .sc-eCApnc').click()
+cy.get('.sc-dlMDgC').click()
+
 });
