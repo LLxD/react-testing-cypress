7a59bd8803a6ebbe26957411f13a8ded8f7ed0d3
Lucas Lima <limalucas454@gmail.com>
2021-10-19 18:22:04 -0300

    adding more changes to desktop and adding mobile version

--- cypress/integration/Add-Produto-Desktop.test.tsx
+++ cypress/integration/Add-Produto-Desktop.test.tsx
@@ -2,21 +2,21 @@ import { mount } from '@cypress/react';
 
 let randomBrandColumn = Math.floor(Math.random() * 4)+1;
 let randomBrand = Math.floor(Math.random() * 4)+1;
-let randomProduct = Math.floor(Math.random() * 8)+1;
+let randomProduct = Math.floor(Math.random() * 7)+1;
 it('Get a random product and add it to the cart - Desktop', () => {
-  cy.log("Iniciando simulação de Login")
+  cy.log("Iniciando simulação de Adição ao Carrinho")
   Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test in case JS loading fails
     return false
   })
   cy.viewport(1280,720)
-  cy.log("Visitando a Página - Desktop")
   cy.visit('/');
   cy.get('.newsletter__popup > .sc-bdnxRM > .sc-hKFxyN > .sc-eCApnc').click()
   cy.get('.unordered-list > :nth-child(1) > a').trigger('mouseover')
   cy.get(`.sc-bUQyIj > :nth-child(${randomBrandColumn}) > :nth-child(${randomBrand})`).click()
+  cy.log("Visitando a Página de Produto - Desktop")
   cy.get(`.prateleira > ul > :nth-child(${randomProduct})`).click()
+  cy.get('.product__add-to-cart')
 
-//   cy.log("Acessando a página de Login - Desktop")
 });
---
+++ cypress/integration/Add-Produto-Mobile.test.tsx
@@ -0,0 +1,27 @@
+import { mount } from '@cypress/react';
+
+
+let randomBrand = Math.floor(Math.random() * 4)+1;
+let randomProduct = Math.floor(Math.random() * 5)+1;
+let randomSize = Math.floor(Math.random() * 2)+1;
+
+it('Goes to offpremium site and tries to login - Mobile', () => {
+  cy.log("Iniciando simulação de Login")
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test in case JS loading fails
+    return false
+  })
+  cy.viewport(375,667)
+  cy.log("Visitando a Página - Mobile")
+  cy.visit('https://www.offpremium.com.br/?uam=true&mobile=2');
+  cy.get('.newsletter__popup > .sc-jrsJWt > .sc-crzoAE > .sc-dIsUp').click()
+  cy.log("Acessando a página de Login - Mobile")
+  // cy.get('.modal-lgpd--btn').click()
+  cy.get('.menu-v2-m__button').click()
+  cy.get(`:nth-child(3) > :nth-child(${randomBrand}) > a > .lazy--load`).click()
+  cy.get(`.prateleira > ul > :nth-child(${randomProduct})`).click()
+  cy.get('.product__fab--button > .product__add-to-cart').click()
+  cy.get(`.interactive-modal__sizes > .slick-list > .slick-track > [data-slick-index="${randomSize}"]`).click()
+  cy.get('.interactive-modal__bottom--cart-btn').click()
+});
