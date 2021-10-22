117fca230c4a93f9df090b02db870c905d36abb7
Lucas Lima <limalucas454@gmail.com>
2021-10-22 11:26:30 -0300

    adding different viewport validations

--- cypress/integration/Add-Produto-Mobile.test.tsx
+++ cypress/integration/Add-Produto-Mobile.test.tsx
@@ -7,63 +7,33 @@ const chance = new Chance();
 let randomBrand = chance.natural({min:1, max:5});
 let randomProduct = chance.natural({min:1, max:6});
 let randomSize = chance.natural({min:1, max:3});
-
-const sizes = [[375, 667], [375, 812], [360, 640]]
-
+const sizes = ['iphone-8','iphone-6','samsung-note9']
 
 
-sizes.forEach((size) => {
-
-
-
-  describe(`Testando o fluxo de adicionar produtos no carrinho para ${size} `,
-  {
-    "viewportWidth": size[0],
-    "viewportHeight": size[1]
-  },
-  ()=>{
-    before(()=> {
-      cy.log("Iniciando simulação de Login")
-      cy.log("Visitando a Página - Mobile")
-      cy.visit('https://www.offpremium.com.br/?uam=true&mobile=2');
+sizes.forEach((size)=>{
+  describe(`Testando o fluxo de adicionar produtos no carrinho para ${size}`,
+  ()=>{  
+    it(`Tries to add a product to the cart in ${size} `, () => {
+      cy.viewport(size)
+      cy.clearCookies()
+      cy.clearLocalStorage()
+      cy.visit('https://www.offpremium.com.br/?uam=true&mobile=2')
       Cypress.on('uncaught:exception', (err, runnable) => {
         // returning false here prevents Cypress from
         // failing the test in case JS loading fails
         return false
       })
-      Cypress.config({
-        viewportWidth: size[0],
-        viewportHeight: size[1],
-      })
-    })
-
-
-    it(`Tries to close the modal - ${size}`, () => {
       cy.get('.newsletter__popup > .sc-jrsJWt > .sc-crzoAE > .sc-dIsUp').click()
       cy.get('.modal-lgpd--btn').click()
-    });
-    
-    it(`Tries to open the menu - ${size}`, () => {
       cy.get('.menu-v2-m__button').click()
-    });
-    
-    it(`Chooses a random brand and navigates to it - ${size}`, () => {
       cy.get(`:nth-child(3) > :nth-child(${randomBrand}) > a > .lazy--load`).click()
-    });
-    
-    it(`Chooses a random product and navigates to it - ${size}`, () => {
       cy.get(`.prateleira > ul > :nth-child(${randomProduct})`).click()
-    });
-    
-    it(`Tries to add the product to cart - ${size}`, () => {
-      cy.get('.product__fab--button > .product__add-to-cart').click()
-      cy.get(`.interactive-modal__sizes > .slick-list > .slick-track > [data-slick-index="${randomSize}"]`).click()
+      cy.get('a').contains("Adicionar à sacola").click()
+      cy.get(`[data-slick-index="${randomSize}"] > .interactive-modal__sizes--size`).click()
       cy.get('.interactive-modal__bottom--cart-btn').click()
-    });
-    it(`Opens the cart and verifies the product is there - ${size}`, () => {
       cy.get('.sc-efHYUO').click()
     });
-
   })
-
 })
+
+ 
